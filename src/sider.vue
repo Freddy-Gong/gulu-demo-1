73f48c955fc6182195bbde8091be8cc2bbbaedc5
Freedy <21914103@zju.edu.cn>
2020-07-15 17:55:28 +0800

    动画未完成

--- src/sider.vue
+++ src/sider.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=flase">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-  name: "GuluSider"
+  name: "GuluSider",
+  data() {
+    return {
+      visible: true
+    };
+  },
+  methods: {}
 };
 </script>
 <style lang="scss"scoped>
 .sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .slider-enter-active,
+  .slider-leave-active {
+    transition: transform 1s;
+  }
+  .slider-enter,
+  .slider-leave-to {
+    transform: translateX(-150px);
+  }
 }
 </style>
